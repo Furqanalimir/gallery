47031af5631b6602bcf878664beb7bb53ee5d93b
furqanalimir <furkaanali89@gmail.com>
2022-07-20 08:47:07 +0530

    ok

--- server.js
+++ server.js
@@ -13,10 +13,16 @@ app.use(cors());
 //connect mongodb
 connectDB();
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use('/api/user', require('./routes/auth'))
 
-if (process?.env?.NODE_ENV === 'production')
-{
+if (process?.env?.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
